d2834679c12ec9e72cb7cdb5546c44dd3ab67803
itggot-erik-jergeus <erik.jergeus@itggot.se>
2016-05-30 09:46:40 +0200

    Added pictures & few css updates
    Fixed a few css bugs and also added pictures for the homepage

--- creation-slutprojekt-sinatra/sinatra-skeleton-master/db/seed.rb
+++ creation-slutprojekt-sinatra/sinatra-skeleton-master/db/seed.rb
@@ -11,22 +11,26 @@ class Seeder
   end
 
   def self.users
-    User.create(username: 'Barn', password: '123ABC', email_address: 'barn.mail@itggot.se', details: 'Hej, Jag heter barn', bed_time: Time.new(0000,01,01,20,00))
-    User.create(username: 'Barn2', password: '123ABC', email_address: 'barn2.mail@itggot.se', details: 'Hej, Jag heter barn2', bed_time: Time.new(0000,01,01,20,00))
+    User.create(username: 'John', password: '123ABC', email_address: 'barn.mail@itggot.se', details: 'Hello, I am John. I use this website because I am not goot at keeping track of homework', bed_time: Time.new(0000,01,01,20,00))
+    User.create(username: 'Mike', password: '123ABC', email_address: 'barn2.mail@itggot.se', details: 'Hello, I am Mike and I am from Sweden', bed_time: Time.new(0000,01,01,20,00))
   end
 
   def self.parents
-    Parent.create(username: 'Parent', password: '123ABC', email_address: 'parent@parent.se', details: 'I am parent')
+    Parent.create(username: 'Rachel', password: '123ABC', email_address: 'parent@parent.se', details: "I am John's mother")
+    Parent.create(username: 'Robert', password: '123ABC', email_address: 'parent2@parent.se', details: "I am John's father and Mike's uncle")
   end
 
   def self.plans
     Plan.create(name: 'Standard', length: 90, amount: 4, weekend: 1, user_id: 1)
-    Plan.create(name: 'Grabb', length: 60, amount: 3, weekend: 1, user_id: 1)
+    Plan.create(name: 'Hourly', length: 60, amount: 3, weekend: 1, user_id: 1)
+    Plan.create(name: 'Standard', length: 90, amount: 4, weekend: 1, user_id: 2)
   end
 
   def self.subjects
-    Subject.create(name: 'Engelska', user_id: 1)
-    Subject.create(name: 'Programmering', user_id: 1)
+    Subject.create(name: 'English', user_id: 1)
+    Subject.create(name: 'Programming', user_id: 1)
+    Subject.create(name: 'Programming', user_id: 2)
+    Subject.create(name: 'Social Studies', user_id: 2)
   end
 
   def self.user_requests
@@ -34,11 +38,14 @@ class Seeder
   end
 
   def self.parent_requests
-    ParentRequest.create(time: DateTime.now, requester_id: 1, user_id: 2)
+    ParentRequest.create(time: DateTime.now, requester_id: 2, user_id: 2)
   end
 
   def self.activities
-    Activity.create(title: "Engelska Prov", type: "Homework", subject: "Engelska", planning: "nil", time: 0, )
+    Activity.create(title: "English essay", type: "Homework", subject: "English", planning: "nil", time: 60, date: (DateTime.now+12), hidden: false, parent: false, user_id: 1)
+    Activity.create(title: "Programming test", type: "Homework", subject: "Programming", planning: "nil", time: 120, date: (DateTime.now+4), hidden: false, parent: false, user_id: 1)
+    Activity.create(title: "Fest hos Mange", type: "Other", subject: "nil", planning: "nil", time: 0, date: (DateTime.now+1), hidden: true, parent: false, user_id: 2)
+    # Suggestion after seed: Create an activity with the standard planning
   end
 
 end
